1e9c7da6dcbbff6c32f5fe458456a0a558cbc0a0
aryanpatil-me <aryanpatil.me@gmail.com>
2025-07-05 11:28:39 +0530

    Add files via upload

---
+++ ldr_alert_system.ino
@@ -0,0 +1,44 @@
+// Variables
+int LDRPin = A0;
+int greenPin = 7;
+int redPin = 6;
+int buzzPin = 5;
+int LDRVal;
+int thresholdVal = 150;
+int delayTime = 250;
+int waitT = 100;
+
+void setup() {
+  // Setting pinMode for Pins:
+  pinMode(LDRPin, INPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(buzzPin, OUTPUT);
+  // Starting serial monitor at 9600 broad rate:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // reading LDR values using analogRead.
+  LDRVal = analogRead(LDRPin);
+  //printing values to get threshold value.
+  Serial.println(LDRVal);
+  delay(delayTime);
+
+  // setting condition for surrounding lights on/off.
+  // for onlight green LED glows and no buzzer sound.
+  // for offlight red LED glows and buzzer start buzzing.
+  if (LDRVal < thresholdVal) {
+    digitalWrite(greenPin, LOW);
+    digitalWrite(redPin, HIGH);
+    digitalWrite(buzzPin, HIGH);
+    delay(waitT);
+    digitalWrite(buzzPin, LOW);
+    delay(waitT);
+
+  } else {
+    digitalWrite(greenPin, HIGH);
+    digitalWrite(redPin, LOW);
+    digitalWrite(buzzPin, LOW);
+  }
+}
